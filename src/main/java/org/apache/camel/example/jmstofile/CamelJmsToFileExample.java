468f469db37898cee345a26e83fb52b315ac73d5
Steve Brenneis <steve.brenneis@secomm.org>
2021-02-04 15:51:42 -0500

    Initial commit

---
+++ src/main/java/org/apache/camel/example/jmstofile/CamelJmsToFileExample.java
@@ -0,0 +1,45 @@
+package org.apache.camel.example.jmstofile;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import javax.jms.ConnectionFactory;
+import java.util.concurrent.TimeUnit;
+
+public class CamelJmsToFileExample {
+
+    public static void main(String[] args) throws Exception {
+
+        // Create default context
+        CamelContext context = new DefaultCamelContext();
+
+        // Set up ActiveMQ JMS components
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+        // Add a named component
+        context.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        // Build the route
+        context.addRoutes(new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("test-jms:queue:test.queue").to("file://test");
+            }
+        });
+
+        // Producer template
+        ProducerTemplate template = context.createProducerTemplate();
+
+        // Don't forget to start the context
+        context.start();
+
+        for (int i = 1; i < 10; i++) {
+            template.sendBody("test-jms:queue:test.queue", "TestMessage: " + i);
+        }
+
+        TimeUnit.SECONDS.sleep(1);
+        context.stop();
+    }
+}
