7018d03a770af55f82c5f31e7a2f76b7b337ca4b
Steve Brenneis <swbrenneis@gmail.com>
2021-02-08 22:21:08 -0500

    Removed Activmq

--- src/main/java/org/apache/camel/example/jmstofile/CamelJmsToFileExample.java
+++ src/main/java/org/apache/camel/example/jmstofile/CamelJmsToFileExample.java
@@ -1,26 +1,23 @@
 package org.apache.camel.example.jmstofile;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.jms.JmsComponent;
 import org.apache.camel.impl.DefaultCamelContext;
 
-import javax.jms.ConnectionFactory;
 import java.util.concurrent.TimeUnit;
 
 public class CamelJmsToFileExample {
 
-    public static void main(String[] args) throws Exception {
+    public void test() throws Exception {
 
         // Create default context
         CamelContext context = new DefaultCamelContext();
 
         // Set up ActiveMQ JMS components
-        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
+//        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
         // Add a named component
-        context.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+//        context.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
         // Build the route
         context.addRoutes(new RouteBuilder() {
             @Override
